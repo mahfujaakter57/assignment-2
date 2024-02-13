14c8a07a34978e30a880ebb58bfc333b753526bb
Mahfuja Akter <mahfujaakter057@gmail.com>
2024-02-13 15:54:59 +0600

    first commit

---
+++ style.js
@@ -0,0 +1,27 @@
+var division = 12/2;
+
+console.log(division);
+
+
+var addition = 9+6;
+
+console.log(addition);
+
+let substraction = 10-2;
+
+console.log(substraction);
+
+{
+    var firstname="Mahfuja";
+       lastname="Akter";
+       fullname=firstname+" "+lastname
+        console.log(fullname)
+       
+}
+
+{let m = 10;
+    m++;
+    let o = m;
+    console.log(o)
+}
+
